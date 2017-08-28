c315b2916089032ac0f030ab4c274e78bdb426ad
Jen <jtrudell@gmail.com>
2017-08-28 09:06:02 -0500

    refactor if statement out of leap solution

--- leap/leap.go
+++ leap/leap.go
@@ -3,8 +3,5 @@ package leap
 const testVersion = 3
 
 func IsLeapYear(year int) bool {
-	if year%4 != 0 || (year%100 == 0 && year%400 != 0) {
-		return false
-	}
-	return true
+	return year%4 == 0 && (year%100 != 0 || year%400 == 0)
 }
